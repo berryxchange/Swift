694e60c0b9e8b1236eee31849d7a248458df7a13
Quinton Quaye <saminoske2@yahoo.com>
2021-03-25 19:15:28 -0500

    uncommented mainView variable for CookieController

--- Testing/CookieFactory/CookieFactoryTests/CookieFactoryTests.swift
+++ Testing/CookieFactory/CookieFactoryTests/CookieFactoryTests.swift
@@ -10,7 +10,7 @@ import XCTest
 
 class CookieFactoryTests: XCTestCase {
 
-    //var mainView = CookieController()
+    var mainView = CookieController()
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
