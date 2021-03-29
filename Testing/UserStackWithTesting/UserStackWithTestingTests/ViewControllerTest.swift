af2a179cf0c280225bef1d099321bd8fb112b36d
Quinton Quaye <saminoske2@yahoo.com>
2021-03-29 04:59:40 -0500

    added extension to viewController tests

--- Testing/UserStackWithTesting/UserStackWithTestingTests/ViewControllerTest.swift
+++ Testing/UserStackWithTesting/UserStackWithTestingTests/ViewControllerTest.swift
@@ -46,5 +46,10 @@ class ViewControllerTest: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+}
 
+extension ViewControllerTest{
+    func testRemovingAUser(){
+        
+    }
 }
